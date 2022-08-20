7e1915284f0c6e00452e17274e45780ae46f7518
Matias Chaves <mc89py@gmail.com>
2022-08-20 17:49:03 -0300

    Login page

---
+++ js/login.js
@@ -0,0 +1,21 @@
+let email = document.getElementById('email')
+let pw = document.getElementById('password')
+let sign_inBtn = document.getElementById('sign_in')
+let alerta = document.getElementById('alerta')
+
+
+
+function input_complete(e) {
+    e.preventDefault()
+    if(email.value.includes('@') && pw.value.length !== 0){
+        window.location.href = "index.html"
+    }else{
+        alert("Algo anda mal! Ingrese los campos correctamente.")
+    }
+}
+
+function onsuccess (){
+    window.location.href = "index.html"
+}
+
+sign_inBtn.addEventListener("click",input_complete)
