16d55c1b74d08c0367b9a2fff0a649aad874df0a
Matias Chaves <mc89py@gmail.com>
2022-10-27 22:06:32 -0300

    rebuild shopping cart

--- js/cart.js
+++ js/cart.js
@@ -1,86 +1,298 @@
+//Variables
+const tableCartHTML = document.getElementById('TableCartHTML');
 
-let tabledata = document.getElementById('cart_tableData');
+const premiumRadio = document.getElementById('premiumradio');
+const expressRadio = document.getElementById('expressradio');
+const standardRadio = document.getElementById('standardradio');
 
-fetch(CART_INFO_URL)
-    .then(res => res.json())
-    .then(cart_usrData =>{
-        console.log(cart_usrData);
-        showArcticles(cart_usrData)
-    })
+const subtotal_cost = document.getElementById('subtotal_span');
+const shipping_cost = document.getElementById('shipping_span');
+const total_cost = document.getElementById('total_span');
+
+const div_radios = document.getElementById('div_radios');
+
+const input_addresses = document.querySelectorAll('.address')
+
+const payment_type = document.getElementById('payment_type');
+
+const modal = document.getElementById('staticBackdrop');
+
+const radio_creditCard = document.getElementById('radio_creditCard');
+const radio_bank_transfer = document.getElementById('bank_transfer');
+
+const card_num = document.getElementById('card_number');
+const card_sec = document.getElementById('card_sec');
+const card_expiration = document.getElementById('card_expiration');
+
+const transf_accountNum = document.getElementById('transf_accountNum');
+
+const form = document.getElementById('form');
+const adress = document.querySelectorAll('adress');
+const form_send = document.getElementById('form_send');
+
+let updatedTotal = 0;
+let cost_of_shipping = 0;
+let initial_cost_of_shipping = 0.15
+let sumOfTotals = 0
 
 
-function showArcticles(cart_usrData){
-    let html_articles = "";
+//event that show totals
+document.addEventListener('DOMContentLoaded',updateTotals)
+//event that change the radios total
+div_radios.addEventListener('change',updateShippingCost)
 
-    let article = cart_usrData.articles[0]
-    console.log(article);
 
-        html_articles = `
-        <tr>
+//display the totals
+function displayTotals (){
+    subtotal_cost.textContent = `USD ${updatedTotal}`
+    shipping_cost.textContent = `USD ${cost_of_shipping}`
+    total_cost.textContent = `USD ${sumOfTotals}`
+}
+
+//updating in realtime when radios change
+function updateShippingCost(){
+    if(premiumRadio.checked){
+        cost_of_shipping = Math.floor(updatedTotal * premiumRadio.value)
+        // console.log(cost_of_shipping);
+        sumOfTotals = (updatedTotal + cost_of_shipping)
+        shipping_cost.textContent = `USD ${cost_of_shipping}`
+        total_cost.textContent = `USD ${sumOfTotals}`
+    }
+    if(expressRadio.checked){
+        cost_of_shipping = Math.floor(updatedTotal * expressRadio.value)
+        // console.log(cost_of_shipping);
+        sumOfTotals = (updatedTotal + cost_of_shipping)
+        shipping_cost.textContent = `USD ${cost_of_shipping}`
+        total_cost.textContent = `USD ${sumOfTotals}`
+    }
+    if(standardRadio.checked){
+        cost_of_shipping = Math.floor(updatedTotal * standardRadio.value)
+        // console.log(cost_of_shipping);
+        sumOfTotals = (updatedTotal + cost_of_shipping)
+        shipping_cost.textContent = `USD ${cost_of_shipping}`
+        total_cost.textContent = `USD ${sumOfTotals}`
+    }
+}
+
+
+function updateTotals() {
+    // consigo un array de los productos en pesos
+    let producto_UYU = local_cart_items.filter(el => el.currency == "UYU")
+
+    let producto_USD = local_cart_items.filter(elem => elem.currency == "USD")
+
+    const sumUSD = producto_USD.reduce((summarize,num)=>{
+        return summarize + num.subtotal
+    },0)
+
+    // convierto los productos en pesos a dolares
+    const sumUYU = producto_UYU.reduce((total,number)=>{
+        return total + (number.subtotal / 40)
+    }, 0)
+
+    
+
+    updatedTotal = sumUSD + sumUYU
+
+    // console.log(updatedTotal);
+    
+    //updating in realtime when the quantity of items change
+    if(premiumRadio.checked){
+        cost_of_shipping = Math.floor(updatedTotal * premiumRadio.value)
+        // console.log(cost_of_shipping);
+        sumOfTotals = updatedTotal + cost_of_shipping
+    }
+    if(expressRadio.checked){
+        cost_of_shipping = Math.floor(updatedTotal * expressRadio.value)
+        // console.log(cost_of_shipping);
+        sumOfTotals = updatedTotal + cost_of_shipping
+    }
+    if(standardRadio.checked){
+        cost_of_shipping = Math.floor(updatedTotal * standardRadio.value)
+        // console.log(cost_of_shipping);
+        sumOfTotals = updatedTotal + cost_of_shipping
+    }
+
+
+    //display the totals
+    displayTotals()
+
+    
+}
+
+
+function deleteItem(id){
+
+    for (let i = 0; i < local_cart_items.length; i++) {
+
+        if(local_cart_items[i].id === id){
+            local_cart_items.splice(i,1)
+            localStorage.setItem('cart',JSON.stringify(local_cart_items))
+            updateTotals()
+
+            
+        }
+    }
+}
+
+if(localStorage.getItem('cart')){
+
+    var local_cart_items = JSON.parse(localStorage.getItem('cart'))
+
+    let cart_tableData = "";
+
+    for(const item of local_cart_items){
+        cart_tableData += `
+        <tr style="vertical-align:middle; text-align:center;">
         <td scope="row">
-            <img src="${article.image}" style="max-width:80px;max-height:50px">
+            <img src="${item.image}" style="max-width:80px;max-height:50px">
         </td>
         <td>
-            <p>${article.name}</p>
+            <p>${item.name}</p>
         </td>
         <td>
-            <p>${article.unitCost}</p>
+            <p>${item.cost}</p>
         </td>
         <td>
-            <input id="articlevalue" class="form-control" style="max-width:60px" type="number" min="1" value="${article.count}">
+            <input class="form-control" id="input_${item.id}" style="max-width:60px" type="number" min="1" value="${item.count}">
+
         </td>
         <td>
-            <p><strong id="unitcost">${article.currency} ${article.unitCost}</strong></p>
+        <strong id="cost${item.id}"> ${item.currency} ${item.cost * item.count} </strong>
+        </td>
+        <td>
+            <button onclick="deleteItem(${item.id})" class="btn" style="border-color:red;"><i class="fa fa-trash-o" style="font-size:25px;color:red"></i></button>
         </td>
         <tr>
         `
 
-        tabledata.innerHTML = html_articles
+        tableCartHTML.innerHTML = cart_tableData
 
-        if(localStorage.getItem('cart_item')){
-            local_cartitems = JSON.parse(localStorage.getItem('cart_item'))
+        console.log(item);
 
-            let cart_td = "";
+    }
+
+
+
+
+    //Event that update the subtotal by quantity 
+    tableCartHTML.addEventListener('change',(e)=>{
+        //target the input value
+        let quantity = e.target.value
+        //target the input id value to set the subtotal id after
+        let quantity_id = e.target.id.slice(6)
+        //setting the subtotal id
+        let subtotal =  document.getElementById('cost'+quantity_id)
+        //finding an elemenent that match the element.id with te quantity.id
+        let product = local_cart_items.find(element => 
+            element.id == quantity_id
+        )
+
+
+
+
+        // console.log(product);
+        //modifying the subtotal content
+        subtotal.textContent = `${product.currency} ${product.cost * quantity}`
+
+        local_cart_items.push(product.subtotal = product.cost * quantity, product.count = parseInt(quantity))
+
+        updateTotals()
+        deleteItem()
+    })
+}
+
+
+
+
+
+
+
+
+function alertSuccess (){
+    document.getElementById('alert-success').classList.add('show')
+}
+
+function alertDanger (){
+    document.getElementById('alert-danger').classList.add('show')
+}
+
+
+function checkModal(){
+    if(radio_creditCard.checked){
+        //remove attribute disabled
+        card_num.removeAttribute('disabled')
+        card_sec.removeAttribute('disabled')
+        card_expiration.removeAttribute('disabled')
         
-            for (const object of local_cartitems) {
-                cart_td += `
-                <tr>
-                <td scope="row">
-                <img src="${object.images[0]}" style="max-width:80px;max-height:50px">
-                </td>
-                <td>
-                    <p>${object.name}</p>
-                </td>
-                <td>
-                    <p>${object.cost}</p>
-                </td>
-                <td>
-                    <input class="form-control" id="${object.name}" style="max-width:60px" type="number" min="1" value="1">
-                </td>
-                <td>
-                    <p><strong id="${object.cost}">${object.currency} ${object.cost}</strong></p>
-                </td>
-                <tr>
-                `
-                tabledata.innerHTML =html_articles + cart_td
-    
-                console.log(object)
-                //change subtotal value local product
-                let cantidad = document.getElementById(`${object.name}`)
-                
-                cantidad.addEventListener('change', () =>{
-                    document.getElementById(`${object.cost}`).innerHTML = `${object.currency} ${object.cost * cantidad.value}`
-                }) 
+        //set the attribute disabled to transf_accountNum
+        transf_accountNum.setAttribute('disabled','')
 
-            }
-        }
+        payment_type.textContent = `Tarjeta de crédito`
+    }
+    if(radio_bank_transfer.checked){
+        //remove attribute disabled
+        transf_accountNum.removeAttribute('disabled')
 
-        //change subtotal value fetched product
-        document.addEventListener('change',()=>{
-            let subtotal = document.getElementById('unitcost')
-            let sumsubtotal = document.getElementById('articlevalue').value * article.unitCost
-            
-            subtotal.innerHTML = `${article.currency} ${sumsubtotal}`
-        })
+        //set the attribute disabled to card_inputs
+        card_num.setAttribute('disabled','')
+        card_sec.setAttribute('disabled','')
+        card_expiration.setAttribute('disabled','')
+
+        payment_type.textContent = `Transferencia bancaria`
+    }
+}
+
+
+function checkModalradios(){
+
+    const payment_modalLink = document.getElementById('payment_modalLink');
+
+    if(radio_creditCard.checked && card_num.value != "" && card_sec.value !="" && card_expiration.value != ""){
+        alertSuccess()
+    }
+
+    if(radio_bank_transfer.checked && transf_accountNum.value !=""){
+        alertSuccess()
+        
+    }
+
+    if(!radio_bank_transfer.checked && radio_creditCard.checked){
+        payment_modalLink.classList.remove('is-invalid')
+        payment_modalLink.classList.add('is-valid')
+    }else if(!radio_creditCard.checked && radio_bank_transfer.checked){
+        payment_modalLink.classList.remove('is-invalid')
+        payment_modalLink.classList.add('is-valid')
+    }else if(!radio_creditCard.checked && !radio_bank_transfer.checked){
+        payment_modalLink.classList.add('is-invalid')
+        alertDanger()
+    }
 
 }
+
+
+function validatecheck(e){
+    e.preventDefault()
+
+
+    form.classList.remove('was-validated');
+    modal.classList.remove('was-validated');
+
+
+    if(!form.checkValidity()){
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    checkModalradios();
+
+    form.classList.add('was-validated');
+    modal.classList.add('was-validated')
+}
+
+
+
+
+form.addEventListener('submit',validatecheck)
+
+modal.addEventListener('change',checkModal)
